75176fd37ce9a31c582710e5e7342de8284c1494
Florian Lefebvre <Florian Lefebvre>
2015-11-24 23:09:56 +0100

    first training

---
+++ Projet-1/exo.py
@@ -0,0 +1,10 @@
+#name = input("entrez un nom: ")
+opening_file = open("data.txt")
+It_is_here = True
+for line in opening_file:
+	words = line.split()
+	age = words [1]
+	if name == name[0]:
+		print(words[1])
+	else:
+		It_is_here = False
